f661a6682abde40fcad2824a387ac9996965de95
Prabhdeep <prabhdepsp@gmail.com>
2026-09-13 20:08:33 +0530

    setting up project

---
+++ Tests/swifty-wasmTests/swifty_wasmTests.swift
@@ -0,0 +1,8 @@
+import Testing
+@testable import swifty_wasm
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    // Swift Testing Documentation
+    // https://swiftpackageindex.com/swiftlang/swift-testing/documentation
+}
