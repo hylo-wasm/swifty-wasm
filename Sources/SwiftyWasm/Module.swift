d349b1304a0a125704b3ce87079059441ab8c332
Prabhdeep <prabhdepsp@gmail.com>
2026-09-15 23:31:36 +0530

    Added function type and Module skeleton.

---
+++ Sources/SwiftyWasm/Module.swift
@@ -0,0 +1,8 @@
+struct Module {
+    //holds a sequence of the functions.
+    var functions : []
+
+    //holds a sequence of the global variables.
+    var globalVariables: []
+
+}
---
+++ Sources/SwiftyWasm/Types/FunctionType.swift
@@ -0,0 +1,8 @@
+struct FunctionType {
+    // holds a sequence of parameters of the function.
+    var parameters : []
+
+    //holds a sequence of return types of the function
+    var returnType: []
+    
+}
