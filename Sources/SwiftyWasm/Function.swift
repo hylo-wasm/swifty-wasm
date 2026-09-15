e332fb8c311245962cb9d356925d59a6318f456b
Prabhdeep <prabhdepsp@gmail.com>
2026-09-16 00:03:54 +0530

    Added instruction and Function

---
+++ Sources/SwiftyWasm/Function.swift
@@ -0,0 +1,7 @@
+struct Function {
+    //holds parameters and return types of the function
+    var signature : FunctionType
+    
+    //holds a sequence of instructions of the function
+    var body: [any Instruction]
+}
