15995566b60ef7e82bd55eb656c31799eb2d7131
Prabhdeep Singh <57953515+PrabhdeepSP@users.noreply.github.com>
2026-09-18 03:15:15 +0530

    Adding types (#2)
    * setting up project
    
    * Added function type and Module skeleton.
    
    * Added instruction and Function
    
    * Added formatting
    
    * removed .vscode
    
    * removed older test folder
    
    * removed unwanted file
    
    * completed function abstraction
    
    * Finished function abstraction
    
    * Added access modifiers in Function abstraction
    
    * added valuetype
    
    * ready to emit function signature
    
    ---------
    
    Co-authored-by: Rishabh Dwivedi <rishabhdwivedi17@gmail.com>

---
+++ Sources/SwiftyWasm/ValueType.swift
@@ -0,0 +1,19 @@
+/// The type of a value at runtime in WebAssembly.
+public enum ValueType {
+
+    /// A 32-bit integer.
+    case i32
+
+    /// A 64-bit integer.
+    case i64
+
+    /// A 32-bit IEEE 754 floating-point.
+    case f32
+
+    /// A 64-bit IEEE 754 floating-point.
+    case f64
+
+    /// A reference to a WebAssembly function.
+    case funcref
+
+}
