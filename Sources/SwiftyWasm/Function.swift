a22c5a8582c3cc8f75970c8d836f23b59a0b758c
Prabhdeep <prabhdepsp@gmail.com>
2026-09-18 02:36:21 +0530

    Added access modifiers in Function abstraction

--- Sources/SwiftyWasm/Function.swift
+++ Sources/SwiftyWasm/Function.swift
@@ -1,21 +1,21 @@
 /// A monomorphic function in wasm bytecode.
-struct Function {
+public struct Function {
 
     /// The type of a function.
-    struct Signature {
+    public struct Signature {
 
         /// The function parameters, in argument-passing order.
-        var parameters: [Int]
+        public let parameters: [Int]
 
         /// The result types of the function, in the order in which values are returned.
-        var returnTypes: [Int]
+        public let returnTypes: [Int]
 
     }
 
     /// Function parameters and return types.
-    var type: Signature
+    public let type: Signature
 
     /// The instructions in the function, in execution order.
-    var body: [any Instruction]
+    public let body: [any Instruction]
 
 }
--- Sources/SwiftyWasm/Module.swift
+++ Sources/SwiftyWasm/Module.swift
@@ -1,6 +1,6 @@
 struct Module {
     //holds a sequence of the functions.
-    var functions: [Int]
+    var functions: [Function]
 
     //holds a sequence of the global variables.
     var globalVariables: [Int]
