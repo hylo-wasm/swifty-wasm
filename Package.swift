4036cc2abe099aa86f34d1711670e3fa36d771fd
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2026-09-22 23:50:52 +0530

    specify formatter (#9)

--- Package.swift
+++ Package.swift
@@ -2,21 +2,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyWasm",
-    products: [
-        .library(
-            name: "SwiftyWasm",
-            targets: ["SwiftyWasm"]
-        )
-    ],
-    targets: [
-        .target(
-            name: "SwiftyWasm"
-        ),
-        .testTarget(
-            name: "SwiftyWasmTests",
-            dependencies: ["SwiftyWasm"]
-        ),
-    ],
-    swiftLanguageModes: [.v6]
+  name: "SwiftyWasm",
+  products: [
+    .library(
+      name: "SwiftyWasm",
+      targets: ["SwiftyWasm"]
+    )
+  ],
+  targets: [
+    .target(
+      name: "SwiftyWasm"
+    ),
+    .testTarget(
+      name: "SwiftyWasmTests",
+      dependencies: ["SwiftyWasm"]
+    ),
+  ],
+  swiftLanguageModes: [.v6]
 )
--- Sources/SwiftyWasm/Function.swift
+++ Sources/SwiftyWasm/Function.swift
@@ -1,21 +1,21 @@
 /// A monomorphic function in wasm bytecode.
 public struct Function {
 
-    /// The type of a function.
-    public struct Signature {
+  /// The type of a function.
+  public struct Signature {
 
-        /// The function parameters, in argument-passing order.
-        public let parameters: [ValueType]
+    /// The function parameters, in argument-passing order.
+    public let parameters: [ValueType]
 
-        /// The result types of the function, in the order in which values are returned.
-        public let returnTypes: [ValueType]
+    /// The result types of the function, in the order in which values are returned.
+    public let returnTypes: [ValueType]
 
-    }
+  }
 
-    /// Function parameters and return types.
-    public let type: Signature
+  /// Function parameters and return types.
+  public let type: Signature
 
-    /// The instructions in the function, in execution order.
-    public let body: [any Instruction]
+  /// The instructions in the function, in execution order.
+  public let body: [any Instruction]
 
 }
